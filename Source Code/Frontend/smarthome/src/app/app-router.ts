6e8bd38efaa1650c0ed9475a964f875d1b491380
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2018-04-18 00:07:21 +0300

    refined header  footer + add routes

---
+++ Source Code/Frontend/smarthome/src/app/app-router.ts
@@ -0,0 +1,34 @@
+import {ModuleWithProviders} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {NotfoundComponent} from './notfound/notfound.component';
+import {HomeComponent} from './home/home.component';
+
+
+// export const router : RouterGlobal = [
+//   {path:'', component: DashboardComponent, children: [
+//       {path:'', redirectTo: 'home', pathMatch:'full'},
+//       {path: 'profile', component: ProfileComponent},
+//       {path:'home', component: HomeComponent},
+//       {path:'play', component: PlayComponent},
+//       {path:'live', component: LiveComponent},
+//       {path:'clubs', component: ClubsComponent},
+//       {path:'learn', component: LearnComponent},
+//       {path:'forum', component: ForumComponent},
+//       {path:'status', component: StatsComponent},
+//       {path:'help', component: HelpComponent}
+//     ]},
+//   {path: 'login', component: LoginComponent},
+//   {path: 'register', component: RegisterComponent},
+//   {path: '**', redirectTo: '/404', pathMatch: 'full'}
+// ];
+
+export const router: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: NotfoundComponent},
+  // {path: 'notfound', component: NotfoundComponent}
+];
+
+// exporter
+export const RouterGlobal: ModuleWithProviders = RouterModule.forRoot(router);
--- Source Code/Frontend/smarthome/src/app/app.module.ts
+++ Source Code/Frontend/smarthome/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import {RouterGlobal} from './app-router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NotfoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterGlobal,
   ],
   providers: [],
   bootstrap: [AppComponent]
